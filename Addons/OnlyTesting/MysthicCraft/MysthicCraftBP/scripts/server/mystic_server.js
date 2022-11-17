56b5bd04b8da45b5a1aeae5b6f5ce562ded36509
Sprintermax <ericfnoe7@hotmail.com>
2022-11-17 10:38:06 -0300

    formated code

--- Addons/OnlyTesting/MysthicCraft/MysthicCraftBP/scripts/server/mystic_server.js
+++ Addons/OnlyTesting/MysthicCraft/MysthicCraftBP/scripts/server/mystic_server.js
@@ -1,33 +1,60 @@
-var system = server.registerSystem(0,0);
+var system = server.registerSystem(0, 0);
 var global = { tick: 0 };
 
-system.initialize = function() {
-	global.viewAllEntities = this.registerQuery();
+system.initialize = function () {
+  global.viewAllEntities = this.registerQuery();
 };
 
 system.update = function () {
-	global.tick += 1;
-    if (global.tick == 5) {
-        let allEntities = system.getEntitiesFromQuery(global.viewAllEntities);
-        let size = allEntities.length;
-        for (let index = 0; index < size; ++index) {
-			if (allEntities[index].__identifier__ == "mystic:forcefield") {
-				let checkActivated = this.getComponent(allEntities[index], "minecraft:health");
-				let forcefieldpos = this.getComponent(allEntities[index], "minecraft:position");
-				if (checkActivated.max == 50) {
-					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §aActivated Forcefield At: §rX: "+forcefieldpos.x+", Y: "+forcefieldpos.y+", Z: "+forcefieldpos.z+"\"}]}");
-					checkActivated.max = 55;
-					this.applyComponentChanges(allEntities[index], checkActivated);
-				} else if (checkActivated.max == 55) {
-					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §2Working...\"}]}");
-				}
-				if (checkActivated.max == 25) {
-					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §aDeactivated Forcefield At: §rX: "+forcefieldpos.x+", Y: "+forcefieldpos.y+", Z: "+forcefieldpos.z+"\"}]}");
-					checkActivated.max = 20;
-					this.applyComponentChanges(allEntities[index], checkActivated);
-				}
-            }
+  global.tick += 1;
+  if (global.tick == 5) {
+    let allEntities = system.getEntitiesFromQuery(global.viewAllEntities);
+    let size = allEntities.length;
+    for (let index = 0; index < size; ++index) {
+      if (allEntities[index].__identifier__ == "mystic:forcefield") {
+        let checkActivated = this.getComponent(
+          allEntities[index],
+          "minecraft:health"
+        );
+        let forcefieldpos = this.getComponent(
+          allEntities[index],
+          "minecraft:position"
+        );
+        if (checkActivated.max == 50) {
+          this.broadcastEvent(
+            "minecraft:execute_command",
+            '/tellraw @a {"rawtext":[{"text":"§d<MythicalExpansion> §aActivated Forcefield At: §rX: ' +
+              forcefieldpos.x +
+              ", Y: " +
+              forcefieldpos.y +
+              ", Z: " +
+              forcefieldpos.z +
+              '"}]}'
+          );
+          checkActivated.max = 55;
+          this.applyComponentChanges(allEntities[index], checkActivated);
+        } else if (checkActivated.max == 55) {
+          this.broadcastEvent(
+            "minecraft:execute_command",
+            '/tellraw @a {"rawtext":[{"text":"§d<MythicalExpansion> §2Working..."}]}'
+          );
         }
-        global.tick = 0;
+        if (checkActivated.max == 25) {
+          this.broadcastEvent(
+            "minecraft:execute_command",
+            '/tellraw @a {"rawtext":[{"text":"§d<MythicalExpansion> §aDeactivated Forcefield At: §rX: ' +
+              forcefieldpos.x +
+              ", Y: " +
+              forcefieldpos.y +
+              ", Z: " +
+              forcefieldpos.z +
+              '"}]}'
+          );
+          checkActivated.max = 20;
+          this.applyComponentChanges(allEntities[index], checkActivated);
+        }
+      }
     }
+    global.tick = 0;
+  }
 };
--- WorkTools/JSONEncodingChanger/index.js
+++ WorkTools/JSONEncodingChanger/index.js
@@ -1,50 +1,60 @@
-
-const path = require('path');
+const path = require("path");
 const fs = require("fs");
 const rl = require("readline").createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question("WorkDir: ", function (d) {
-	if (!d) d = './';
-	rl.question("Mode: ", function (m) {
-		rl.close();
-		td(d, m);
-		console.log("Done. Press any key to close.");
-		process.stdin.resume();
-		process.stdin.setRawMode(true);
-		process.stdin.on('data', process.exit);
-	});
+  if (!d) d = "./";
+  rl.question("Mode: ", function (m) {
+    rl.close();
+    td(d, m);
+    console.log("Done. Press any key to close.");
+    process.stdin.resume();
+    process.stdin.setRawMode(true);
+    process.stdin.on("data", process.exit);
+  });
 });
 
 function td(d, mde) {
-	fs.readdirSync(d).forEach(File => {
-		const abs = path.join(d, File);
-		if (fs.statSync(abs).isDirectory()) return td(abs);
-		else if (abs.endsWith('.json')) {
-			const dt = JSON.parse(fs.readFileSync(abs).toString("UTF-8").replace(/[\n\r\t]/g, ""))
-			return fs.writeFileSync(abs, JSON.stringify(Wj(dt, mde)).replace(/\\\\u/g, '\\u'));
-		}
-	});
+  fs.readdirSync(d).forEach((File) => {
+    const abs = path.join(d, File);
+    if (fs.statSync(abs).isDirectory()) return td(abs);
+    else if (abs.endsWith(".json")) {
+      const dt = JSON.parse(
+        fs
+          .readFileSync(abs)
+          .toString("UTF-8")
+          .replace(/[\n\r\t]/g, "")
+      );
+      return fs.writeFileSync(
+        abs,
+        JSON.stringify(Wj(dt, mde)).replace(/\\\\u/g, "\\u")
+      );
+    }
+  });
 }
 
 function Cvt(str) {
-	const nstr = [];
-	if (typeof str === 'object' && str !== null) return str;
-	if (!isNaN(str)) return str;
-	for (let i = 0, strLen = str.length; i < strLen; i++) nstr.push('\\u' + parseInt(str.charCodeAt(i), 10).toString(16).padStart(4, '0'));
-	return nstr.join('');
+  const nstr = [];
+  if (typeof str === "object" && str !== null) return str;
+  if (!isNaN(str)) return str;
+  for (let i = 0, strLen = str.length; i < strLen; i++)
+    nstr.push(
+      "\\u" + parseInt(str.charCodeAt(i), 10).toString(16).padStart(4, "0")
+    );
+  return nstr.join("");
 }
 
 function Wj(i, md) {
-	if (md == 'rvt') return i;
-	if (typeof i !== 'object') return Cvt(i);
-	if (Array.isArray(i)) return i.map(el => Wj(el));
-	return Object.keys(i).reduce(function (nobj, k) {
-		const vl = i[k];
-		const nvl = (typeof vl === 'object') && vl !== null ? Wj(vl) : vl;
-		nobj[Cvt(k)] = Cvt(nvl);
-		return nobj;
-	}, {});
+  if (md == "rvt") return i;
+  if (typeof i !== "object") return Cvt(i);
+  if (Array.isArray(i)) return i.map((el) => Wj(el));
+  return Object.keys(i).reduce(function (nobj, k) {
+    const vl = i[k];
+    const nvl = typeof vl === "object" && vl !== null ? Wj(vl) : vl;
+    nobj[Cvt(k)] = Cvt(nvl);
+    return nobj;
+  }, {});
 }
